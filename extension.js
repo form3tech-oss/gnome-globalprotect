2d3dc93d04f0cc31f8221f987b1eed891a6a9233
Sascha Hagedorn <sascha.hagedorn@form3.tech>
2021-08-23 15:13:18 +0200

    Grey out icon if VPN is disconnected

--- extension.js
+++ extension.js
@@ -1,50 +1,108 @@
+const Lang = imports.lang;
 const St = imports.gi.St;
 const Gio = imports.gi.Gio;
-
-const ExtensionUtils = imports.misc.extensionUtils;
-const Lang = imports.lang;
 const GLib = imports.gi.GLib;
-const Me = ExtensionUtils.getCurrentExtension();
+const ExtensionUtils = imports.misc.extensionUtils;
+const ThisExtension = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
+const ByteArray = imports.byteArray;
 
 const SHOW_UI_COMMAND = "globalprotect launch-ui";
-const GLOBAL_PROTECT_ICON_PATH = "/usr/share/icons/hicolor/48x48/apps/globalprotect.png";
+const IS_CONNECTED_COMMAND = "ip link show gpd0";
+
+const CHECK_CONNECTION_INTERVAL_MS = 5000;
+
+const CONNECTED_ICON = Gio.icon_new_for_string(ThisExtension.path + "/icons/connected.png");
+const DISCONNECTED_ICON = Gio.icon_new_for_string(ThisExtension.path + "/icons/disconnected.png");
+
+const connectedIcon = new St.Icon({ gicon: CONNECTED_ICON, style_class: 'system-status-icon' });
+const disconnectedIcon = new St.Icon({ gicon: DISCONNECTED_ICON, style_class: 'system-status-icon' });
 
 let globalProtectUILauncher;
+let clearInterval = GLib.Source.remove;
+
+function setInterval(func, delay, ...args) {
+    const wrappedFunc = () => {
+        return func.apply(this, args) || true;
+    };
+    return GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, wrappedFunc);
+}
 
 const GlobalProtectUILauncher = new Lang.Class({
-    Name: `${Me.metadata.name}`,
+    Name: `${ThisExtension.metadata.name}`,
+
+    connectionMonitor: null,
+    launcherButton: null,
+    icon: disconnectedIcon,
 
     _init: function() {
-        let launcherName = `${Me.metadata.name} UI Launcher`;
+        let launcherName = `${ThisExtension.metadata.name} UI Launcher`;
         
-        let launcherButton = new PanelMenu.Button(0.0, launcherName, false);
-        
-        let gicon = this._loadGlobalProtectIcon();
-        let icon = new St.Icon({ gicon: gicon, style_class: 'system-status-icon' });
-        launcherButton.actor.connect('button_press_event', Lang.bind(this, this._showui, false))
-        launcherButton.add_child(icon);
+        this.launcherButton = new PanelMenu.Button(0.0, launcherName, false);
+        this.launcherButton.actor.connect('button_press_event', Lang.bind(this, this.showui, false))
+        this.launcherButton.add_child(this.icon);
+        Main.panel.addToStatusArea(launcherName, this.launcherButton);
 
-        Main.panel.addToStatusArea(launcherName, launcherButton);
+        this.connectionMonitor = setInterval(Lang.bind(this, this.checkConnection, false), CHECK_CONNECTION_INTERVAL_MS);
     },
 
-    _showui: function() {
-        this._spawn(SHOW_UI_COMMAND);
+    showui: function() {
+        this._spawn(SHOW_UI_COMMAND, null);
     },
 
-    _spawn: function(cmd) {
-        log(`spawning command ${cmd}`);
+    checkConnection: function() {
+        this._spawn(IS_CONNECTED_COMMAND, Lang.bind(this, function(output) {
+            if (output !== "" && output.includes("UP")) {
+                this.launcherButton.remove_child(this.icon);
+                this.icon = connectedIcon;
+                this.launcherButton.add_child(this.icon)
+            } else {
+                this.launcherButton.remove_child(this.icon);
+                this.icon = disconnectedIcon;
+                this.launcherButton.add_child(this.icon);
+            }
+        }, false));
+    },
+
+    _spawn: function(cmd, resultCallback) {
+        let success, argv, pid, in_fd, out_fd, err_fd;
+        [success,argv] = GLib.shell_parse_argv(cmd);
 
         try {
-            GLib.spawn_command_line_async(cmd);
-        } catch (err) {
+            [success, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(
+                null,
+                argv,
+                null,
+                GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+                null);
+        } catch(err) {
             logError(err);
+        }
+        
+        if (success && pid != 0) {
+            let stdout = new Gio.DataInputStream({ 
+                base_stream: new Gio.UnixInputStream({fd: out_fd}) 
+            });
+    
+            GLib.child_watch_add( GLib.PRIORITY_DEFAULT, pid,
+                function(pid) {
+                    GLib.spawn_close_pid(pid);
+                    var [line, size, buf] = [null, 0, ""];
+                    while (([line, size] = stdout.read_line(null)) != null && line != null) {
+                        buf += ByteArray.toString(line);
+                    }
+                  
+                    typeof resultCallback == 'function' && resultCallback(buf);
+                }
+            );
         }
     },
 
-    _loadGlobalProtectIcon: function() {
-        return Gio.icon_new_for_string(GLOBAL_PROTECT_ICON_PATH);
+    destroy: function() {
+        if (this.connectionMonitor != null) {
+            clearInterval(this.connectionMonitor);
+        }
     }
 });
 
@@ -53,12 +111,12 @@ class Extension {
     }
 
     enable() {
-        log(`enabling ${Me.metadata.name}`);
+        log(`enabling ${ThisExtension.metadata.name}`);
         globalProtectUILauncher = new GlobalProtectUILauncher();
     }
 
     disable() {
-        log(`disabling ${Me.metadata.name}`);
+        log(`disabling ${ThisExtension.metadata.name}`);
 
         globalProtectUILauncher.destroy();
         globalProtectUILauncher = null;
@@ -66,6 +124,6 @@ class Extension {
 }
 
 function init() {
-    log(`initializing ${Me.metadata.name}`);
+    log(`initializing ${ThisExtension.metadata.name}`);
     return new Extension();
 }
